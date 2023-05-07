b40db8dd3ab162c363d5ba67989d07dc17864021
Nhan Vo <nhoxben335@gmail.com>
2023-05-07 17:53:55 -0300

    Added port listener

--- server/index.js
+++ server/index.js
@@ -7,3 +7,10 @@ connectToDatabase();
 const app = express();
 
 app.use(express.json());
+
+// Add port listener
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
