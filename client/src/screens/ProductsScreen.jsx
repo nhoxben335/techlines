0a64eae6302e4d0156a7761ce2b46070813adac9
Nhan Vo <nhoxben335@gmail.com>
2023-05-08 17:48:14 -0300

    fetched products from database

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,8 +1,20 @@
 import { Center, Wrap, WrapItem } from "@chakra-ui/react";
 import ProductCard from "../components/ProductCard";
-import { products } from "../products";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../redux/actions/productActions";
+// import { products } from "../products";
+import { useEffect } from "react";
 
 const ProductsScreen = () => {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.products);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   return (
     <Wrap spacing="30px" justify="center" minHeight="100vh">
       {products.map((product) => (
