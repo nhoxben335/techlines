61def38930ec10a083e6aa3885c2a86c8aaeb0d9
Nhan Vo <nhoxben335@gmail.com>
2023-05-19 10:39:05 -0300

    12 Loading spinner

---
+++ client/src/screens/CartScreen.jsx
@@ -0,0 +1,22 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Link,
+  Stack,
+  useColorModeValue,
+  Spinner,
+  Alert,
+  AlertTitle,
+  AlertIcon,
+  AlertDescription,
+  Wrap,
+} from "@charkra-ui/react";
+import { Link as ReactLink } from "react-router-dom";
+
+const CartScreen = () => {
+  return <div>CartScreen</div>;
+};
+
+export default CartScreen;
--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,4 +1,14 @@
-import { Center, Wrap, WrapItem } from "@chakra-ui/react";
+import {
+  Center,
+  Wrap,
+  WrapItem,
+  Spinner,
+  Stack,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
 import ProductCard from "../components/ProductCard";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../redux/actions/productActions";
@@ -17,13 +27,32 @@ const ProductsScreen = () => {
 
   return (
     <Wrap spacing="30px" justify="center" minHeight="100vh">
-      {products.map((product) => (
-        <WrapItem key={product._id}>
-          <Center w="250px" h="550px">
-            <ProductCard product={product} />
-          </Center>
-        </WrapItem>
-      ))}
+      {loading ? (
+        <Stack direction="row" spacing={4}>
+          <Spinner
+            mt={20}
+            thickness="2px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="orange.500"
+            size="xl"
+          />
+        </Stack>
+      ) : error ? (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>We are sorry!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      ) : (
+        products.map((product) => (
+          <WrapItem key={product._id}>
+            <Center w="250px" h="550px">
+              <ProductCard product={product} />
+            </Center>
+          </WrapItem>
+        ))
+      )}
     </Wrap>
   );
 };
